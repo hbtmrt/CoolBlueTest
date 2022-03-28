fa0acf09ab41cb834ad1d0f74072a0a08cb94dd7
Haritha Tennakoon <haritha.tennakoon@protendering.com>
2022-03-28 10:59:10 +0530

    Task-02: Refactoring: Added more test cases

---
+++ tests/Insurance.Tests/ControllerTestStartup.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace Insurance.Tests
+{
+    public class ControllerTestStartup
+    {
+        public List<dynamic> Products => GetProducts();
+
+        private List<dynamic> GetProducts()
+        {
+            List<dynamic> products = new List<dynamic>
+            {
+                GenerateSaleBelow500(),
+                GenerateSaleBetween500and2000(),
+                GenerateSaleAbove2000(),
+                GenerateSaleBelow500AndTypeOfLaptop(),
+                GenerateSaleBetween500and2000AndTypeOfSmartphone(),
+                GenerateSaleAbove2000AndTypeOfLaptop(),
+            };
+
+            return products;
+        }
+
+        private dynamic GenerateSaleAbove2000()
+        {
+            return new
+            {
+                id = 3,
+                name = "Test Product above 2000",
+                productTypeId = 1,
+                salesPrice = 2004
+            };
+        }
+
+        private dynamic GenerateSaleBetween500and2000()
+        {
+            return new
+            {
+                id = 2,
+                name = "Test Product between 500 and 2000",
+                productTypeId = 1,
+                salesPrice = 899
+            };
+        }
+
+        private dynamic GenerateSaleBelow500()
+        {
+            return new
+            {
+                id = 1,
+                name = "Test Product below 500",
+                productTypeId = 1,
+                salesPrice = 366
+            };
+        }
+
+        private dynamic GenerateSaleBelow500AndTypeOfLaptop()
+        {
+            return new
+            {
+                id = 4,
+                name = "Test Product below 500 and type of laptop",
+                productTypeId = 2,
+                salesPrice = 400
+            };
+        }
+
+        private dynamic GenerateSaleBetween500and2000AndTypeOfSmartphone()
+        {
+            return new
+            {
+                id = 5,
+                name = "Test Product between 500 and 2000 and type of smartphone",
+                productTypeId = 3,
+                salesPrice = 1345
+            };
+        }
+
+        private dynamic GenerateSaleAbove2000AndTypeOfLaptop()
+        {
+            return new
+            {
+                id = 6,
+                name = "Test Product above 2000 and type of laptop",
+                productTypeId = 2,
+                salesPrice = 4456
+            };
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseRouting();
+            app.UseEndpoints(
+                ep =>
+                {
+                    ep.MapGet(
+                        "products/{id:int}",
+                        context =>
+                        {
+                            int productId = int.Parse((string)context.Request.RouteValues["id"]);
+                            var product = this.Products.FirstOrDefault(p => p.id == productId);
+                            return context.Response.WriteAsync(JsonConvert.SerializeObject((object)product));
+                        }
+                    );
+                    ep.MapGet(
+                        "product_types",
+                        context =>
+                        {
+                            var productTypes = new[]
+                                               {
+                                                   new
+                                                   {
+                                                       id = 1,
+                                                       name = "Test type",
+                                                       canBeInsured = true
+                                                   },
+                                                   new
+                                                   {
+                                                       id = 2,
+                                                       name = "Laptops",
+                                                       canBeInsured = true
+                                                   },
+                                                   new
+                                                   {
+                                                       id = 3,
+                                                       name = "Smartphones",
+                                                       canBeInsured = true
+                                                   }
+                                               };
+                            return context.Response.WriteAsync(JsonConvert.SerializeObject(productTypes));
+                        }
+                    );
+                }
+            );
+        }
+    }
+}
--- tests/Insurance.Tests/UnitTest1.cs
+++ tests/Insurance.Tests/UnitTest1.cs
@@ -1,15 +1,6 @@
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using Insurance.Api.Controllers;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Newtonsoft.Json;
 using Xunit;
 
 namespace Insurance.Tests
@@ -105,144 +96,19 @@ namespace Insurance.Tests
                 actual: insurance
             );
         }
-    }
 
-    public class ControllerTestFixture : IDisposable
-    {
-        private readonly IHost _host;
-
-        public ControllerTestFixture()
+        [Fact]
+        public void CalculateInsurance_GivenSalesPriceAbove2000Euros_AndTypeOfLaptop_ShouldAdd2000EurosToInsuranceCost()
         {
-            _host = new HostBuilder()
-                   .ConfigureWebHostDefaults(
-                        b => b.UseUrls("http://localhost:5002")
-                              .UseStartup<ControllerTestStartup>()
-                    )
-                   .Build();
+            const float expectedInsuranceValue = 2500;
 
-            _host.Start();
-        }
+            HomeController homeController = new HomeController(_configuration);
 
-        public void Dispose() => _host.Dispose();
-    }
+            float insurance = homeController.CalculateInsuranceAsync(6).Result;
 
-    public class ControllerTestStartup
-    {
-        public List<dynamic> Products => GetProducts();
-
-        private List<dynamic> GetProducts()
-        {
-            List<dynamic> products = new List<dynamic>
-            {
-                GenerateSaleBelow500(),
-                GenerateSaleBetween500and2000(),
-                GenerateSaleAbove2000(),
-                GenerateSaleBelow500AndTypeOfLaptop(),
-                GenerateSaleBetween500and2000AndTypeOfSmartphone(),
-            };
-
-            return products;
-        }
-
-        private dynamic GenerateSaleAbove2000()
-        {
-            return new
-            {
-                id = 3,
-                name = "Test Product above 2000",
-                productTypeId = 1,
-                salesPrice = 2004
-            };
-        }
-
-        private dynamic GenerateSaleBetween500and2000()
-        {
-            return new
-            {
-                id = 2,
-                name = "Test Product between 500 and 2000",
-                productTypeId = 1,
-                salesPrice = 899
-            };
-        }
-
-        private dynamic GenerateSaleBelow500()
-        {
-            return new
-            {
-                id = 1,
-                name = "Test Product below 500",
-                productTypeId = 1,
-                salesPrice = 366
-            };
-        }
-
-        private dynamic GenerateSaleBelow500AndTypeOfLaptop()
-        {
-            return new
-            {
-                id = 4,
-                name = "Test Product below 500 and type of laptop",
-                productTypeId = 2,
-                salesPrice = 400
-            };
-        }
-
-        private dynamic GenerateSaleBetween500and2000AndTypeOfSmartphone()
-        {
-            return new
-            {
-                id = 5,
-                name = "Test Product between 500 and 2000 and type of smartphone",
-                productTypeId = 3,
-                salesPrice = 1345
-            };
-        }
-
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseRouting();
-            app.UseEndpoints(
-                ep =>
-                {
-                    ep.MapGet(
-                        "products/{id:int}",
-                        context =>
-                        {
-                            int productId = int.Parse((string)context.Request.RouteValues["id"]);
-                            var product = this.Products.FirstOrDefault(p => p.id == productId);
-                            return context.Response.WriteAsync(JsonConvert.SerializeObject((object)product));
-                        }
-                    );
-                    ep.MapGet(
-                        "product_types",
-                        context =>
-                        {
-                            var productTypes = new[]
-                                               {
-                                                   new
-                                                   {
-                                                       id = 1,
-                                                       name = "Test type",
-                                                       canBeInsured = true
-                                                   },
-                                                   new
-                                                   {
-                                                       id = 2,
-                                                       name = "Laptops",
-                                                       canBeInsured = true
-                                                   },
-                                                   new
-                                                   {
-                                                       id = 3,
-                                                       name = "Smartphones",
-                                                       canBeInsured = true
-                                                   }
-                                               };
-                            return context.Response.WriteAsync(JsonConvert.SerializeObject(productTypes));
-                        }
-                    );
-                }
+            Assert.Equal(
+                expected: expectedInsuranceValue,
+                actual: insurance
             );
         }
     }
